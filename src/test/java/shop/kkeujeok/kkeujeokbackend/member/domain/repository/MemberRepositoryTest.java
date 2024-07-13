b6fca970137382b4e66b4e9227927b00db2b18fc
inhooo00 <chldlsgh0987@naver.com>
2024-07-14 00:17:23 +0900

    Feat(#20): MemberRepository 테스트 작성

--- src/test/java/shop/kkeujeok/kkeujeokbackend/member/domain/repository/MemberRepositoryTest.java
+++ src/test/java/shop/kkeujeok/kkeujeokbackend/member/domain/repository/MemberRepositoryTest.java
@@ -1,30 +1,39 @@
-//package shop.kkeujeok.kkeujeokbackend.member.domain.repository;
-//
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-//import org.springframework.transaction.annotation.Transactional;
-//import shop.kkeujeok.kkeujeokbackend.member.domain.Member;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//
-//@DataJpaTest
-//public class MemberRepositoryTest {
-//
-//    @Autowired
-//    private MemberRepository memberRepository;
-//
-//    @DisplayName("이메일을 통해 회원을 찾는다.")
-//    @Test
-//    @Transactional
-//    void 이메일을_통해_회원을_찾는다() {
-//        Member 끄적 = Member.builder()
-//                .email("끄적_이메일")
-//                .build();
-//
-//        memberRepository.save(끄적);
-//
-//        assertThat(memberRepository.findByEmail("끄적_이메일").get().getId()).isEqualTo(끄적.getId());
-//    }
-//}
+package shop.kkeujeok.kkeujeokbackend.member.domain.repository;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import shop.kkeujeok.kkeujeokbackend.member.domain.Member;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class MemberRepositoryTest {
+
+    @Mock
+    private MemberRepository memberRepository;
+
+    @Test
+    void testFindByEmail() {
+        // given
+        String email = "test@example.com";
+        Member member = Member.builder()
+                .email(email)
+                .build();
+
+        when(memberRepository.findByEmail(anyString())).thenReturn(Optional.of(member));
+
+        // when
+        Optional<Member> foundMember = memberRepository.findByEmail(email);
+
+        // then
+        assertThat(foundMember).isPresent();
+        assertThat(foundMember.get().getEmail()).isEqualTo(email);
+    }
+}
