7599f98441003bb8ff56c1ec35ee59703a0f73b8
giwoong <dev.choi0409@gmail.com>
2024-07-25 19:45:51 +0900

    Feat(#11): Block 상태별 전체 조회, 단건 조회 기능 구현

--- src/main/java/shop/kkeujeok/kkeujeokbackend/block/application/BlockService.java
+++ src/main/java/shop/kkeujeok/kkeujeokbackend/block/application/BlockService.java
@@ -1,16 +1,23 @@
 package shop.kkeujeok.kkeujeokbackend.block.application;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import shop.kkeujeok.kkeujeokbackend.block.api.dto.request.BlockSaveReqDto;
 import shop.kkeujeok.kkeujeokbackend.block.api.dto.request.BlockUpdateReqDto;
 import shop.kkeujeok.kkeujeokbackend.block.api.dto.response.BlockInfoResDto;
+import shop.kkeujeok.kkeujeokbackend.block.api.dto.response.BlockListResDto;
 import shop.kkeujeok.kkeujeokbackend.block.domain.Block;
 import shop.kkeujeok.kkeujeokbackend.block.domain.Progress;
 import shop.kkeujeok.kkeujeokbackend.block.domain.repository.BlockRepository;
 import shop.kkeujeok.kkeujeokbackend.block.exception.BlockNotFoundException;
 import shop.kkeujeok.kkeujeokbackend.block.exception.InvalidProgressException;
+import shop.kkeujeok.kkeujeokbackend.global.dto.PageInfoResDto;
 import shop.kkeujeok.kkeujeokbackend.member.domain.Member;
 import shop.kkeujeok.kkeujeokbackend.member.domain.repository.MemberRepository;
 
@@ -18,6 +25,7 @@ import shop.kkeujeok.kkeujeokbackend.member.domain.repository.MemberRepository;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class BlockService {
+    private static final Logger log = LoggerFactory.getLogger(BlockService.class);
     private final MemberRepository memberRepository;
     private final BlockRepository blockRepository;
 
@@ -38,10 +46,7 @@ public class BlockService {
         Member member = Member.builder().nickname("member").build();
         Block block = blockRepository.findById(blockId).orElseThrow(BlockNotFoundException::new);
 
-        if (!block.getTitle().equals(blockUpdateReqDto.title()) ||
-                !block.getContents().equals(blockUpdateReqDto.contents())) {
-            block.update(blockUpdateReqDto.title(), blockUpdateReqDto.contents());
-        }
+        block.update(blockUpdateReqDto.title(), blockUpdateReqDto.contents(), blockUpdateReqDto.deadLine());
 
         return BlockInfoResDto.of(block, member);
     }
@@ -60,6 +65,30 @@ public class BlockService {
         return BlockInfoResDto.of(block, member);
     }
 
+    // 블록 리스트
+    public BlockListResDto findByBlockWithProgress(String progress, Pageable pageable) {
+        Member member = Member.builder().nickname("member").build();
+        Page<Block> blocks = blockRepository.findByBlockWithProgress(parseProgress(progress), pageable);
+
+        List<BlockInfoResDto> blockInfoResDtoList = blocks.stream()
+                .map(b -> BlockInfoResDto.of(b, member))
+                .toList();
+
+        return BlockListResDto.from(blockInfoResDtoList, PageInfoResDto.from(blocks));
+    }
+
+    // 블록 상세보기
+    public BlockInfoResDto findById(Long blockId) {
+        // 로그인/회원가입 코드 완성 후 사용자 정보 받아올 예정
+        Member member = Member.builder().nickname("member").build();
+        Block block = blockRepository.findById(blockId).orElseThrow(BlockNotFoundException::new);
+
+        return BlockInfoResDto.of(block, member);
+    }
+
+    // 블록 삭제 (논리 삭제)
+
+
     private Progress parseProgress(String progressString) {
         try {
             return Progress.valueOf(progressString);
@@ -67,11 +96,5 @@ public class BlockService {
             throw new InvalidProgressException();
         }
     }
-
-    // 블록 삭제 (논리 삭제)
-
-    // 블록 리스트
-
-    // 블록 상세보기
 
 }
