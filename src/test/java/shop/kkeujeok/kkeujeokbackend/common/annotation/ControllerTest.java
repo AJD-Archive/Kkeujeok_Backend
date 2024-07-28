3f1784875da497ff97a649343492b9ff85e70fb5
inhooo00 <chldlsgh0987@naver.com>
2024-07-28 21:10:28 +0900

    Test: TokenProvider 목킹 의존성 주입 추가

--- src/test/java/shop/kkeujeok/kkeujeokbackend/common/annotation/ControllerTest.java
+++ src/test/java/shop/kkeujeok/kkeujeokbackend/common/annotation/ControllerTest.java
@@ -1,18 +1,26 @@
 package shop.kkeujeok.kkeujeokbackend.common.annotation;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import shop.kkeujeok.kkeujeokbackend.auth.api.AuthController;
+import shop.kkeujeok.kkeujeokbackend.auth.application.AuthMemberService;
+import shop.kkeujeok.kkeujeokbackend.auth.application.AuthService;
+import shop.kkeujeok.kkeujeokbackend.auth.application.AuthServiceFactory;
+import shop.kkeujeok.kkeujeokbackend.auth.application.TokenService;
 import shop.kkeujeok.kkeujeokbackend.block.api.BlockController;
 import shop.kkeujeok.kkeujeokbackend.block.application.BlockService;
+import shop.kkeujeok.kkeujeokbackend.global.jwt.TokenProvider;
 
 @AutoConfigureRestDocs
 @WebMvcTest({
-        BlockController.class
+        BlockController.class,
+        AuthController.class
 })
 @ActiveProfiles("test")
 public abstract class ControllerTest {
@@ -25,4 +33,20 @@ public abstract class ControllerTest {
 
     @MockBean
     protected BlockService blockService;
+
+    @MockBean
+    protected TokenProvider tokenProvider;
+
+    @MockBean
+    protected AuthServiceFactory authServiceFactory;
+
+    @MockBean
+    protected AuthMemberService authMemberService;
+
+    @MockBean
+    protected TokenService tokenService;
+
+    @Mock
+    protected AuthService authService;
+
 }
