91fb9f3b894b97578ea38b6063cd4476c67f9b81
inhooo00 <chldlsgh0987@naver.com>
2024-08-04 23:34:33 +0900

    Refactor(#48): 닉네임 중복 가능하게 로직 수정

--- src/main/java/shop/kkeujeok/kkeujeokbackend/member/nickname/application/NicknameService.java
+++ src/main/java/shop/kkeujeok/kkeujeokbackend/member/nickname/application/NicknameService.java
@@ -1,5 +1,6 @@
 package shop.kkeujeok.kkeujeokbackend.member.nickname.application;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import shop.kkeujeok.kkeujeokbackend.member.domain.repository.MemberRepository;
@@ -9,42 +10,16 @@ import java.util.List;
 import java.util.Random;
 
 @Service
+@RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class NicknameService {
+
     private final List<String> adjectives;
     private final List<String> nouns;
     private final Random random;
-    private final MemberRepository memberRepository;
-
-    public NicknameService(List<String> adjectives, List<String> nouns, Random random, MemberRepository memberRepository) {
-        this.adjectives = adjectives;
-        this.nouns = nouns;
-        this.random = random;
-        this.memberRepository = memberRepository;
-    }
 
     public String getRandomNickname() {
-        int maxAttempts = getMaxAttempts();
-        return generateUniqueNickname(maxAttempts);
-    }
-
-    private int getMaxAttempts() {
-        return adjectives.size() * nouns.size();
-    }
-
-    private String generateUniqueNickname(int maxAttempts) {
-        int attempts = 0;
-        String nickname;
-
-        do {
-            if (attempts >= maxAttempts) {
-                throw new TimeOutException();
-            }
-            nickname = generateNickname();
-            attempts++;
-        } while (isNicknameUsed(nickname));
-
-        return nickname;
+        return generateNickname();
     }
 
     private String generateNickname() {
@@ -56,10 +31,4 @@ public class NicknameService {
     private String getRandomElement(List<String> list) {
         return list.get(random.nextInt(list.size()));
     }
-
-    private boolean isNicknameUsed(String nickname) {
-        return memberRepository.existsByNickname(nickname);
-    }
-
-    // 닉네임은 중복 가능하고, 새로운 컬럼을 만들어서 고유 숫자태그를 부여. 이 값은 마이페이지에서 확인 가능.
 }
