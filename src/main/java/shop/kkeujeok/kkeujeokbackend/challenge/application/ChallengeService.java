2eaf377acce577192fb9cf510a8d63f171ed1f64
dongkyunKim <kyun010713@gmail.com>
2024-08-04 22:05:48 +0900

    Refactor():챌린지 생성 메서드 명 변경

--- src/main/java/shop/kkeujeok/kkeujeokbackend/challenge/application/ChallengeService.java
+++ src/main/java/shop/kkeujeok/kkeujeokbackend/challenge/application/ChallengeService.java
@@ -3,25 +3,27 @@ package shop.kkeujeok.kkeujeokbackend.challenge.application;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import shop.kkeujeok.kkeujeokbackend.auth.exception.EmailNotFoundException;
 import shop.kkeujeok.kkeujeokbackend.challenge.api.dto.reqeust.ChallengeSaveReqDto;
 import shop.kkeujeok.kkeujeokbackend.challenge.api.dto.response.ChallengeInfoResDto;
 import shop.kkeujeok.kkeujeokbackend.challenge.domain.Challenge;
 import shop.kkeujeok.kkeujeokbackend.challenge.domain.repository.ChallengeRepository;
 import shop.kkeujeok.kkeujeokbackend.member.domain.Member;
 import shop.kkeujeok.kkeujeokbackend.member.domain.repository.MemberRepository;
+import shop.kkeujeok.kkeujeokbackend.member.exception.MemberNotFoundException;
 
 @Service
 @RequiredArgsConstructor
 public class ChallengeService {
+
     private final ChallengeRepository challengeRepository;
     private final MemberRepository memberRepository;
 
     @Transactional
-    public ChallengeInfoResDto create(String email, ChallengeSaveReqDto challengeSaveReqDto) {
-        Member member = memberRepository.findByEmail(email).orElseThrow(EmailNotFoundException::new);
+    public ChallengeInfoResDto save(String email, ChallengeSaveReqDto challengeSaveReqDto) {
+        Member member = memberRepository.findByEmail(email).orElseThrow(MemberNotFoundException::new);
         Challenge challenge = challengeSaveReqDto.toEntity(member);
         challenge.validateCycleDetails(challengeSaveReqDto.cycle());
+
         challengeRepository.save(challenge);
 
         return ChallengeInfoResDto.of(challenge, member);
