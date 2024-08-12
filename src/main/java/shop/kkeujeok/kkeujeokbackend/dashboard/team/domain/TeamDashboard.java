d052fd6003f30f9974453c6a57ca7c25fcb50350
giwoong <dev.choi0409@gmail.com>
2024-08-12 22:27:24 +0900

    Feat(#67): 팀 대시보드 도메인 구현

---
+++ src/main/java/shop/kkeujeok/kkeujeokbackend/dashboard/team/domain/TeamDashboard.java
@@ -0,0 +1,28 @@
+package shop.kkeujeok.kkeujeokbackend.dashboard.team.domain;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import shop.kkeujeok.kkeujeokbackend.dashboard.domain.Dashboard;
+import shop.kkeujeok.kkeujeokbackend.member.domain.Member;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class TeamDashboard extends Dashboard {
+
+    @OneToMany(mappedBy = "teamDashboard", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<TeamDashboardMemberMapping> teamDashboardMemberMappings = new ArrayList<>();
+
+    @Builder
+    private TeamDashboard(String title, String description, Member member) {
+        super(title, description, member);
+    }
+
+}
