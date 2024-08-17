eec15d590c9e53d7955609e05d0b40f952b65fca
inhooo00 <chldlsgh0987@naver.com>
2024-08-17 20:53:09 +0900

    Test(#70): DocumentRepositoryTest 구현

--- src/test/java/shop/kkeujeok/kkeujeokbackend/dashboard/teamdocument/domain/repository/DocumentRepositoryTest.java
+++ src/test/java/shop/kkeujeok/kkeujeokbackend/dashboard/teamdocument/domain/repository/DocumentRepositoryTest.java
@@ -12,6 +12,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.ActiveProfiles;
+import shop.kkeujeok.kkeujeokbackend.dashboard.team.domain.TeamDashboard;
+import shop.kkeujeok.kkeujeokbackend.dashboard.team.domain.repository.TeamDashboardRepository;
 import shop.kkeujeok.kkeujeokbackend.dashboard.teamdocument.domain.Document;
 import shop.kkeujeok.kkeujeokbackend.global.config.JpaAuditingConfig;
 import shop.kkeujeok.kkeujeokbackend.global.config.QuerydslConfig;
@@ -24,24 +26,37 @@ class DocumentRepositoryTest {
     @Autowired
     private DocumentRepository documentRepository;
 
+    @Autowired
+    private TeamDashboardRepository teamDashboardRepository;
+
+    private TeamDashboard teamDashboard;
     private Document document1;
     private Document document2;
     private Document document3;
 
     @BeforeEach
     void setUp() {
+        teamDashboard = TeamDashboard.builder()
+                .title("title")
+                .description("description")
+                .build();
+
         document1 = Document.builder()
                 .title("Document Title 1")
+                .teamDashboard(teamDashboard)
                 .build();
 
         document2 = Document.builder()
                 .title("Document Title 2")
+                .teamDashboard(teamDashboard)
                 .build();
 
         document3 = Document.builder()
                 .title("Document Title 3")
+                .teamDashboard(teamDashboard)
                 .build();
 
+        teamDashboardRepository.save(teamDashboard);
         documentRepository.save(document1);
         documentRepository.save(document2);
         documentRepository.save(document3);
@@ -52,14 +67,13 @@ class DocumentRepositoryTest {
     void 팀_대시보드_ID로_문서를_전체_조회합니다() {
         // given
         Pageable pageable = PageRequest.of(0, 10);
-        Long teamDashboardId = 1L; // 가정: document 엔티티에 teamDashboardId 필드가 있다면
 
         // when
-        Page<Document> documents = documentRepository.findByDocumentWithTeamDashboard(teamDashboardId, pageable);
+        Page<Document> documents = documentRepository.findByDocumentWithTeamDashboard(teamDashboard.getId(), pageable);
 
         // then
         assertThat(documents.getContent().size()).isEqualTo(3);
-        assertThat(documents.getContent()).extracting("teamDashboardId").containsOnly(teamDashboardId);
+        assertThat(documents.getContent()).extracting("teamDashboard.Id").containsOnly(teamDashboard.getId());
     }
 
     @DisplayName("문서를 논리 삭제 상태별로 전체 조회합니다.")
@@ -67,12 +81,12 @@ class DocumentRepositoryTest {
     void 문서를_논리_삭제_상태별로_전체_조회합니다() {
         // given
         Pageable pageable = PageRequest.of(0, 10);
-        document1.statusUpdate(); // 문서를 논리 삭제 상태로 업데이트
+        document1.statusUpdate();
 
         // when
-        Page<Document> documents = documentRepository.findByDocumentWithTeamDashboard(1L, pageable); // 가정: document 엔티티에 teamDashboardId 필드가 있다면
+        Page<Document> documents = documentRepository.findByDocumentWithTeamDashboard(teamDashboard.getId(), pageable);
 
         // then
-        assertThat(documents.getContent().size()).isEqualTo(2); // 논리 삭제 상태가 아닌 문서만 조회
+        assertThat(documents.getContent().size()).isEqualTo(2);
     }
 }
