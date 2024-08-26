53f69dcd9db0e93cf836e8734fd00ff2ea66b148
dongkyunKim <kyun010713@gmail.com>
2024-08-26 12:07:10 +0900

    Feat(#74): 파라미터 name 설정

--- src/main/java/shop/kkeujeok/kkeujeokbackend/notification/api/NotificationController.java
+++ src/main/java/shop/kkeujeok/kkeujeokbackend/notification/api/NotificationController.java
@@ -1,11 +1,12 @@
 package shop.kkeujeok.kkeujeokbackend.notification.api;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import shop.kkeujeok.kkeujeokbackend.global.annotation.CurrentUserEmail;
@@ -27,14 +28,17 @@ public class NotificationController {
     }
 
     @GetMapping
-    public RspTemplate<NotificationListResDto> getAllNotifications(@CurrentUserEmail String email, Pageable pageable) {
-        NotificationListResDto notificationList = notificationService.findAllNotificationsFromMember(email, pageable);
-        return new RspTemplate<>(HttpStatus.OK, "알림 조회 성공", notificationList);
+    public RspTemplate<NotificationListResDto> findAllNotifications(@CurrentUserEmail String email,
+                                                                    @RequestParam(defaultValue = "0", name = "page") int page,
+                                                                    @RequestParam(defaultValue = "10", name = "size") int size) {
+        return new RspTemplate<>(HttpStatus.OK, "알림 조회 성공",
+                notificationService.findAllNotificationsFromMember(email, PageRequest.of(page, size)));
     }
 
     @GetMapping("/{notificationId}")
-    public RspTemplate<NotificationInfoResDto> getNotificationById(@PathVariable Long notificationId) {
-        NotificationInfoResDto notification = notificationService.findByNotificationId(notificationId);
-        return new RspTemplate<>(HttpStatus.OK, "알림 상세 조회 성공", notification);
+    public RspTemplate<NotificationInfoResDto> findNotificationById(
+            @PathVariable(name = "notificationId") Long notificationId) {
+        return new RspTemplate<>(HttpStatus.OK, "알림 상세 조회 성공",
+                notificationService.findByNotificationId(notificationId));
     }
 }
