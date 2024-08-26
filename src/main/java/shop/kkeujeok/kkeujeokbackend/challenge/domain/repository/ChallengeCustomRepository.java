2b19846216ea5d7f66288ac99dea5fecd53e8f7b
dongkyunKim <kyun010713@gmail.com>
2024-08-26 16:34:26 +0900

    Feat(#81): 카테고리 검색 메서드 추가

--- src/main/java/shop/kkeujeok/kkeujeokbackend/challenge/domain/repository/ChallengeCustomRepository.java
+++ src/main/java/shop/kkeujeok/kkeujeokbackend/challenge/domain/repository/ChallengeCustomRepository.java
@@ -3,10 +3,13 @@ package shop.kkeujeok.kkeujeokbackend.challenge.domain.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import shop.kkeujeok.kkeujeokbackend.challenge.api.dto.reqeust.ChallengeSearchReqDto;
+import shop.kkeujeok.kkeujeokbackend.challenge.domain.Category;
 import shop.kkeujeok.kkeujeokbackend.challenge.domain.Challenge;
 
 public interface ChallengeCustomRepository {
     Page<Challenge> findAllChallenges(Pageable pageable);
 
     Page<Challenge> findChallengesByKeyWord(ChallengeSearchReqDto challengeSearchReqDto, Pageable pageable);
+
+    Page<Challenge> findChallengesByCategory(Category category, Pageable pageable);
 }
