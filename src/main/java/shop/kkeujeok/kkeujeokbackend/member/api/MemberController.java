d982dfa380593e7c5309a4ea76bad5499099c0ad
inhooo00 <chldlsgh0987@naver.com>
2024-08-26 15:13:02 +0900

    Feat(#48): 마이페이지 챌린지, 팀 대시보드 조회

--- src/main/java/shop/kkeujeok/kkeujeokbackend/member/api/MemberController.java
+++ src/main/java/shop/kkeujeok/kkeujeokbackend/member/api/MemberController.java
@@ -1,11 +1,13 @@
 package shop.kkeujeok.kkeujeokbackend.member.api;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import shop.kkeujeok.kkeujeokbackend.global.annotation.CurrentUserEmail;
 import shop.kkeujeok.kkeujeokbackend.global.template.RspTemplate;
 import shop.kkeujeok.kkeujeokbackend.member.mypage.api.dto.request.MyPageUpdateReqDto;
 import shop.kkeujeok.kkeujeokbackend.member.mypage.api.dto.response.MyPageInfoResDto;
+import shop.kkeujeok.kkeujeokbackend.member.mypage.api.dto.response.TeamDashboardsAndChallengesResDto;
 import shop.kkeujeok.kkeujeokbackend.member.mypage.application.MyPageService;
 import shop.kkeujeok.kkeujeokbackend.member.nickname.application.NicknameService;
 
@@ -14,6 +16,7 @@ import java.util.Set;
 @RestController
 @RequestMapping("/api/members")
 public class MemberController {
+
     private final MyPageService myPageService;
     private final NicknameService nicknameService;
 
@@ -34,4 +37,10 @@ public class MemberController {
         return new RspTemplate<>(HttpStatus.OK, "내 프로필 정보 수정", myPageService.update(email, myPageUpdateReqDto));
     }
 
+    @GetMapping("/mypage/dashboard-challenges")
+    public RspTemplate<TeamDashboardsAndChallengesResDto> getTeamDashboardsAndChallenges(@CurrentUserEmail String email,
+                                                                                         Pageable pageable) {
+        TeamDashboardsAndChallengesResDto response = myPageService.findTeamDashboardsAndChallenges(email, pageable);
+        return new RspTemplate<>(HttpStatus.OK, "팀 대시보드와 챌린지 정보 조회", response);
+    }
 }
--- src/main/java/shop/kkeujeok/kkeujeokbackend/member/mypage/application/MyPageService.java
+++ src/main/java/shop/kkeujeok/kkeujeokbackend/member/mypage/application/MyPageService.java
@@ -1,22 +1,31 @@
 package shop.kkeujeok.kkeujeokbackend.member.mypage.application;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import shop.kkeujeok.kkeujeokbackend.auth.exception.EmailNotFoundException;
+import shop.kkeujeok.kkeujeokbackend.challenge.api.dto.response.ChallengeListResDto;
+import shop.kkeujeok.kkeujeokbackend.challenge.application.ChallengeService;
+import shop.kkeujeok.kkeujeokbackend.challenge.domain.repository.ChallengeRepository;
+import shop.kkeujeok.kkeujeokbackend.dashboard.team.api.dto.response.TeamDashboardListResDto;
+import shop.kkeujeok.kkeujeokbackend.dashboard.team.application.TeamDashboardService;
+import shop.kkeujeok.kkeujeokbackend.dashboard.team.domain.repository.TeamDashboardRepository;
 import shop.kkeujeok.kkeujeokbackend.member.domain.Member;
 import shop.kkeujeok.kkeujeokbackend.member.domain.repository.MemberRepository;
 import shop.kkeujeok.kkeujeokbackend.member.mypage.api.dto.request.MyPageUpdateReqDto;
 import shop.kkeujeok.kkeujeokbackend.member.mypage.api.dto.response.MyPageInfoResDto;
+import shop.kkeujeok.kkeujeokbackend.member.mypage.api.dto.response.TeamDashboardsAndChallengesResDto;
 import shop.kkeujeok.kkeujeokbackend.member.mypage.exception.ExistsNicknameException;
 
 @Service
+@RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class MyPageService {
-    private final MemberRepository memberRepository;
 
-    public MyPageService(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
+    private final MemberRepository memberRepository;
+    private final TeamDashboardService teamDashboardService;
+    private final ChallengeService challengeService;
 
     // 프로필 정보 조회
     public MyPageInfoResDto findMyProfileByEmail(String email) {
@@ -51,7 +60,14 @@ public class MyPageService {
         return nickname.replaceAll("\\s+", "");
     }
 
-    // 팀 대시보드 / 챌린지 정보 조회
+    // 팀 대시보드 & 챌린지 정보 조회
+    @Transactional(readOnly = true)
+    public TeamDashboardsAndChallengesResDto findTeamDashboardsAndChallenges(String email, Pageable pageable) {
+        TeamDashboardListResDto teamDashboardListResDto = teamDashboardService.findForTeamDashboard(email, pageable);
+        ChallengeListResDto challengeListResDto = challengeService.findAllChallenges(pageable);
+
+        return TeamDashboardsAndChallengesResDto.of(teamDashboardListResDto, challengeListResDto);
+    }
 
     // 알림 조회
 }
