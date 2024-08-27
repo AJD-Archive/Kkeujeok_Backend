e798b08be22ce0f7ac4878cb9e628c4a3ec7899b
DongkyunKim <134477764+dongkyun0713@users.noreply.github.com>
2024-08-27 20:35:47 +0900

    Feat(#81): 챌린지 카테고리 추가 (#83)
    * Feat(#74): 카테고리 검색 메서드 추가
    
    * Test(#81):
    
    * Feat(#81): 카테고리 검색 메서드 추가
    
    * Feat(#81): 카테고리 검색 메서드 추가
    
    * Feat(#81): 카테고리 클래스 추가
    
    * Feat(#81): 챌린지 엔티티에 카테고리 필드 추가
    
    * Feat(#81): 카테고리 필드 추가
    
    * Feat(#81): 카테고리 검색 메서드 추가
    
    * Feat(#81): 카테고리 필드 추가
    
    * Test(#81): 챌린지 카테고리 검색 테스트
    
    * Fix(#81): 카테고리 타입 변경
    
    * Fix(#81): 카테고리 타입 변경
    
    * Fix(#81): 카테고리 타입 변경

--- src/main/java/shop/kkeujeok/kkeujeokbackend/challenge/api/dto/response/ChallengeInfoResDto.java
+++ src/main/java/shop/kkeujeok/kkeujeokbackend/challenge/api/dto/response/ChallengeInfoResDto.java
@@ -3,6 +3,7 @@ package shop.kkeujeok.kkeujeokbackend.challenge.api.dto.response;
 import java.time.LocalDate;
 import java.util.List;
 import lombok.Builder;
+import shop.kkeujeok.kkeujeokbackend.challenge.domain.Category;
 import shop.kkeujeok.kkeujeokbackend.challenge.domain.Challenge;
 import shop.kkeujeok.kkeujeokbackend.challenge.domain.Cycle;
 import shop.kkeujeok.kkeujeokbackend.challenge.domain.CycleDetail;
@@ -11,6 +12,7 @@ import shop.kkeujeok.kkeujeokbackend.challenge.domain.CycleDetail;
 public record ChallengeInfoResDto(
         String title,
         String contents,
+        Category category,
         Cycle cycle,
         List<CycleDetail> cycleDetails,
         LocalDate startDate,
@@ -23,6 +25,7 @@ public record ChallengeInfoResDto(
         return ChallengeInfoResDto.builder()
                 .title(challenge.getTitle())
                 .contents(challenge.getContents())
+                .category(challenge.getCategory())
                 .cycle(challenge.getCycle())
                 .cycleDetails(challenge.getCycleDetails())
                 .startDate(challenge.getStartDate())
