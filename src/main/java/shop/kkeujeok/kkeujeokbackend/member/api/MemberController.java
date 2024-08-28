5cc0a6cc164b1a0f855d7d9c0ba73fbf49b20a5d
choi inho <129029251+inhooo00@users.noreply.github.com>
2024-08-28 11:15:20 +0900

    Feat(#48): 마이페이지 챌린지, 팀 대시보드 조회 (#82)
    * Feat(#48): 마이페이지 챌린지, 팀 대시보드 조회
    
    * Refactor(#48): 컨벤션 리팩토링
    
    * Refactor(#48): 컨벤션 리팩토링
    
    * Refactor(#48): 어노테이션 순서 수정
    
    * Feat(#48): 챌린지 이메일로 조회하는 로직 추가, 마이페이지에서 조회하기 로직 추가
    
    * Refactor(#48): 줄바꿈 수정
    
    * Test(#48): 마이페이지 팀 대시보드, 챌린지 조회 테스트코드, 문서 작업

--- src/main/java/shop/kkeujeok/kkeujeokbackend/member/api/MemberController.java
+++ src/main/java/shop/kkeujeok/kkeujeokbackend/member/api/MemberController.java
@@ -1,26 +1,24 @@
 package shop.kkeujeok.kkeujeokbackend.member.api;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import shop.kkeujeok.kkeujeokbackend.global.annotation.CurrentUserEmail;
 import shop.kkeujeok.kkeujeokbackend.global.template.RspTemplate;
 import shop.kkeujeok.kkeujeokbackend.member.mypage.api.dto.request.MyPageUpdateReqDto;
 import shop.kkeujeok.kkeujeokbackend.member.mypage.api.dto.response.MyPageInfoResDto;
+import shop.kkeujeok.kkeujeokbackend.member.mypage.api.dto.response.TeamDashboardsAndChallengesResDto;
 import shop.kkeujeok.kkeujeokbackend.member.mypage.application.MyPageService;
-import shop.kkeujeok.kkeujeokbackend.member.nickname.application.NicknameService;
 
 import java.util.Set;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/members")
 public class MemberController {
-    private final MyPageService myPageService;
-    private final NicknameService nicknameService;
 
-    public MemberController(MyPageService myPageService, NicknameService nicknameService) {
-        this.myPageService = myPageService;
-        this.nicknameService = nicknameService;
-    }
+    private final MyPageService myPageService;
 
     @GetMapping("/mypage")
     public RspTemplate<MyPageInfoResDto> myProfileInfo(@CurrentUserEmail String email) {
@@ -34,4 +32,10 @@ public class MemberController {
         return new RspTemplate<>(HttpStatus.OK, "내 프로필 정보 수정", myPageService.update(email, myPageUpdateReqDto));
     }
 
+    @GetMapping("/mypage/dashboard-challenges")
+    public RspTemplate<TeamDashboardsAndChallengesResDto> getTeamDashboardsAndChallenges(@CurrentUserEmail String email,
+                                                                                         Pageable pageable) {
+        TeamDashboardsAndChallengesResDto response = myPageService.findTeamDashboardsAndChallenges(email, pageable);
+        return new RspTemplate<>(HttpStatus.OK, "팀 대시보드와 챌린지 정보 조회", response);
+    }
 }
