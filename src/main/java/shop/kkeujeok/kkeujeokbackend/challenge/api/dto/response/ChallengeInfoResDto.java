11349d0aa960411556c708f7de2463efc11bdf48
DongkyunKim <134477764+dongkyun0713@users.noreply.github.com>
2024-09-10 17:33:52 +0900

    Refactor(#104): Response에 챌린지 id 추가 (#105)
    * Feat(#74): 카테고리 검색 메서드 추가
    
    * Test(#81):
    
    * Feat(#81): 카테고리 검색 메서드 추가
    
    * Feat(#81): 카테고리 검색 메서드 추가
    
    * Feat(#81): 카테고리 클래스 추가
    
    * Feat(#81): 챌린지 엔티티에 카테고리 필드 추가
    
    * Feat(#81): 카테고리 필드 추가
    
    * Feat(#81): 카테고리 검색 메서드 추가
    
    * Feat(#81): 카테고리 필드 추가
    
    * Test(#81): 챌린지 카테고리 검색 테스트
    
    * Fix(#81): 카테고리 타입 변경
    
    * Fix(#81): 카테고리 타입 변경
    
    * Fix(#81): 카테고리 타입 변경
    
    * Docs(#90): enum 항목 설명 추가
    
    * Refactor(#97): 매직 넘버 상수로 변환
    
    * Refactor(#104): responseDto에 challengeId 추가

--- src/main/java/shop/kkeujeok/kkeujeokbackend/challenge/api/dto/response/ChallengeInfoResDto.java
+++ src/main/java/shop/kkeujeok/kkeujeokbackend/challenge/api/dto/response/ChallengeInfoResDto.java
@@ -10,6 +10,7 @@ import shop.kkeujeok.kkeujeokbackend.challenge.domain.CycleDetail;
 
 @Builder
 public record ChallengeInfoResDto(
+        Long challengeId,
         String title,
         String contents,
         Category category,
@@ -23,6 +24,7 @@ public record ChallengeInfoResDto(
 ) {
     public static ChallengeInfoResDto from(Challenge challenge) {
         return ChallengeInfoResDto.builder()
+                .challengeId(challenge.getId())
                 .title(challenge.getTitle())
                 .contents(challenge.getContents())
                 .category(challenge.getCategory())
