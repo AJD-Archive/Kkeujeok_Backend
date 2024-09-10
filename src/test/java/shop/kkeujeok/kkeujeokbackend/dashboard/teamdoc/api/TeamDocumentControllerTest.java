c32ef5d64fcf1339b763e0c43c832ae6bcc06058
inhooo00 <chldlsgh0987@naver.com>
2024-09-10 17:35:32 +0900

    Test(#106): 팀 문서 컨트롤러 수정 중..

--- src/test/java/shop/kkeujeok/kkeujeokbackend/dashboard/teamdoc/api/TeamDocumentControllerTest.java
+++ src/test/java/shop/kkeujeok/kkeujeokbackend/dashboard/teamdoc/api/TeamDocumentControllerTest.java
@@ -1,9 +1,9 @@
 package shop.kkeujeok.kkeujeokbackend.dashboard.teamdoc.api;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
 import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
@@ -16,6 +16,7 @@ import static org.springframework.restdocs.request.RequestDocumentation.paramete
 import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.restdocs.request.RequestDocumentation.queryParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static shop.kkeujeok.kkeujeokbackend.global.restdocs.RestDocsHandler.requestFields;
 import static shop.kkeujeok.kkeujeokbackend.global.restdocs.RestDocsHandler.responseFields;
@@ -24,13 +25,17 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import shop.kkeujeok.kkeujeokbackend.auth.api.dto.request.TokenReqDto;
 import shop.kkeujeok.kkeujeokbackend.common.annotation.ControllerTest;
 import shop.kkeujeok.kkeujeokbackend.dashboard.teamdoc.api.dto.request.FindTeamDocumentReqDto;
 import shop.kkeujeok.kkeujeokbackend.dashboard.teamdoc.api.dto.request.TeamDocumentReqDto;
@@ -38,7 +43,9 @@ import shop.kkeujeok.kkeujeokbackend.dashboard.teamdoc.api.dto.request.TeamDocum
 import shop.kkeujeok.kkeujeokbackend.dashboard.teamdoc.api.dto.response.FindTeamDocumentResDto;
 import shop.kkeujeok.kkeujeokbackend.dashboard.teamdoc.api.dto.response.TeamDocumentResDto;
 import shop.kkeujeok.kkeujeokbackend.dashboard.teamdoc.application.TeamDocumentService;
+import shop.kkeujeok.kkeujeokbackend.global.annotationresolver.CurrentUserEmailArgumentResolver;
 import shop.kkeujeok.kkeujeokbackend.global.dto.PageInfoResDto;
+import shop.kkeujeok.kkeujeokbackend.global.error.ControllerAdvice;
 import shop.kkeujeok.kkeujeokbackend.global.template.RspTemplate;
 
 import java.util.Collections;
@@ -50,21 +57,24 @@ import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuild
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 
 import org.springframework.test.web.servlet.MockMvc;
+import shop.kkeujeok.kkeujeokbackend.member.domain.repository.MemberRepository;
 
 class TeamDocumentControllerTest extends ControllerTest {
 
     @InjectMocks
-    TeamDocumentController teamDocumentController;
+    private TeamDocumentController teamDocumentController;
 
-    private MockMvc mockMvc;
-
-    @InjectMocks
+    @MockBean
     private TeamDocumentService teamDocumentService;
 
     @BeforeEach
     void setUp(RestDocumentationContextProvider restDocumentation) {
+        ReflectionTestUtils.setField(teamDocumentController, "teamDocumentService", teamDocumentService);
+
         mockMvc = MockMvcBuilders.standaloneSetup(teamDocumentController)
+                .setCustomArgumentResolvers(new CurrentUserEmailArgumentResolver(tokenProvider))
                 .apply(documentationConfiguration(restDocumentation))
+                .setControllerAdvice(new ControllerAdvice())
                 .build();
     }
 
@@ -72,18 +82,23 @@ class TeamDocumentControllerTest extends ControllerTest {
     @Test
     void save() throws Exception {
         // given
-        TeamDocumentReqDto request = new TeamDocumentReqDto("author", "title","content","category",1L);
-        TeamDocumentResDto response = new TeamDocumentResDto("author", "picture","title","category",1L);
+        TeamDocumentReqDto request = new TeamDocumentReqDto("author", "title", "content", "category", 1L);
+        TeamDocumentResDto response = new TeamDocumentResDto("author", "picture", "title", "category", 1L);
 
-        given(teamDocumentService.save(any(), any(TeamDocumentReqDto.class))).willReturn(response);
+        // Mocking
+        given(teamDocumentService.save(anyString(), any(TeamDocumentReqDto.class))).willReturn(new TeamDocumentResDto("author", "picture", "title", "category", 1L));
 
+        TeamDocumentResDto response1 = new TeamDocumentResDto("author", "picture", "title", "category", 1L);
+        System.out.println("Response DTO: " + response1);  // 생성된 응답 객체 확인
+        String json = objectMapper.writeValueAsString(response);
+        System.out.println("Serialized JSON: " + json);
         // when & then
         mockMvc.perform(post("/api/dashboards/team/document")
                         .header("Authorization", "Bearer valid-token")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(request)))
                 .andDo(print())
-                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data").isNotEmpty())
                 .andDo(document("document/save",
                         preprocessRequest(prettyPrint()),
                         preprocessResponse(prettyPrint()),
@@ -91,16 +106,23 @@ class TeamDocumentControllerTest extends ControllerTest {
                                 headerWithName("Authorization").description("JWT 토큰")
                         ),
                         requestFields(
+                                fieldWithPath("title").description("문서 제목"),
+                                fieldWithPath("content").description("문서 내용"),
+                                fieldWithPath("category").description("문서 카테고리"),
                                 fieldWithPath("teamDashboardId").description("팀 대시보드 ID"),
-                                fieldWithPath("title").description("문서 제목")
+                                fieldWithPath("author").description("문서 작성자")
                         ),
                         responseFields(
                                 fieldWithPath("statusCode").description("상태 코드"),
                                 fieldWithPath("message").description("응답 메시지"),
-                                fieldWithPath("data.documentId").description("문서 ID"),
-                                fieldWithPath("data.title").description("문서 제목")
+                                fieldWithPath("data.title").description("문서 제목"),
+                                fieldWithPath("data.author").description("문서 글쓴이"),
+                                fieldWithPath("data.picture").description("문서 글쓴이 사진"),
+                                fieldWithPath("data.category").description("문서 카테고리"),
+                                fieldWithPath("data.documentId").description("문서 ID")
                         )
-                ));
+                ))
+                .andExpect(status().isOk());
     }
 
     @DisplayName("PATCH 팀 문서 수정 테스트")
@@ -144,7 +166,7 @@ class TeamDocumentControllerTest extends ControllerTest {
     void findTeamDocumentByCategory() throws Exception {
         // given
         List<TeamDocumentResDto> teamDocuments = List.of(
-                new TeamDocumentResDto("author1", "picture1", "title1", "category1",  1L),
+                new TeamDocumentResDto("author1", "picture1", "title1", "category1", 1L),
                 new TeamDocumentResDto("author2", "picture2", "title2", "category2", 2L)
         );
 
