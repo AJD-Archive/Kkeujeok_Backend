a8d73d1c186f77de3250650535f7cbbd43b07b90
dongkyunKim <kyun010713@gmail.com>
2024-09-24 21:30:40 +0900

    Fix(#170): 챌린지 생성 안되는 오류 해결

--- src/main/java/shop/kkeujeok/kkeujeokbackend/challenge/application/ChallengeService.java
+++ src/main/java/shop/kkeujeok/kkeujeokbackend/challenge/application/ChallengeService.java
@@ -22,6 +22,7 @@ import shop.kkeujeok.kkeujeokbackend.challenge.api.dto.response.ChallengeInfoRes
 import shop.kkeujeok.kkeujeokbackend.challenge.api.dto.response.ChallengeListResDto;
 import shop.kkeujeok.kkeujeokbackend.challenge.domain.Challenge;
 import shop.kkeujeok.kkeujeokbackend.challenge.domain.ChallengeMemberMapping;
+import shop.kkeujeok.kkeujeokbackend.challenge.domain.CycleDetail;
 import shop.kkeujeok.kkeujeokbackend.challenge.domain.repository.ChallengeRepository;
 import shop.kkeujeok.kkeujeokbackend.challenge.exception.ChallengeAccessDeniedException;
 import shop.kkeujeok.kkeujeokbackend.challenge.exception.ChallengeNotFoundException;
@@ -160,7 +161,7 @@ public class ChallengeService {
     private Block createBlock(Challenge challenge, Member member, Dashboard personalDashboard) {
         return Block.builder()
                 .title(challenge.getTitle())
-                .contents(challenge.getContents())
+                .contents(generateCycleDescription(challenge))
                 .progress(Progress.NOT_STARTED)
                 .type(Type.CHALLENGE)
                 .startDate(LocalDate.now()
@@ -172,6 +173,14 @@ public class ChallengeService {
                 .challenge(challenge)
                 .build();
     }
+
+    private String generateCycleDescription(Challenge challenge) {
+        return challenge.getCycle().getDescription() + " " +
+                challenge.getCycleDetails().stream()
+                        .map(CycleDetail::getDescription)
+                        .collect(Collectors.joining(", "));
+    }
+
 
     @Transactional(readOnly = true)
     public ChallengeListResDto findChallengeForMemberId(String email, Pageable pageable) {
