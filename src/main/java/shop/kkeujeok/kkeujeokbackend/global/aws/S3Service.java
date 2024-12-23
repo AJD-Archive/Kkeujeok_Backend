d457665ba118a1ed8c20699351ab6d326eac3527
DongkyunKim <134477764+dongkyun0713@users.noreply.github.com>
2024-12-23 16:29:04 +0900

    Fix: 이미지 업로드 안 되는 오류 해결 (#252)
    * Refactor: 리펙토링
    
    * fix(#251): 이미지 버킷 이름 변경

--- src/main/java/shop/kkeujeok/kkeujeokbackend/global/aws/S3Service.java
+++ src/main/java/shop/kkeujeok/kkeujeokbackend/global/aws/S3Service.java
@@ -16,7 +16,7 @@ public class S3Service {
 
     private final S3Client s3Client;
 
-    private static final String BUCKET_NAME = "kkeujeok-image-bucket";
+    private static final String BUCKET_NAME = "kkeujeok-s3-images-bucket";
     private static final String IMAGE_PATH = "challenge-images/";
     private static final String S3_URL_FORMAT = "https://%s.s3.amazonaws.com/%s";
     private static final String IMAGE_EXTENSION = ".jpg";
