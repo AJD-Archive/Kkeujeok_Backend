afb9535330fd2865b976ae63fdfb922709c80cf5
choigiwoong <dev.choi0409@gmail.com>
2025-01-12 00:06:37 +0900

    Refactor: amdin 패키지 삭제 (#259)

--- src/main/java/shop/kkeujeok/kkeujeokbackend/amdin/domain/repository/NoticeRepository.java
+++
@@ -1,7 +0,0 @@
-package shop.kkeujeok.kkeujeokbackend.amdin.domain.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import shop.kkeujeok.kkeujeokbackend.amdin.domain.Notice;
-
-public interface NoticeRepository extends JpaRepository<Notice, Long> {
-}
